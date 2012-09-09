ec79ecef8329ca4644f16eba61cdb591c11350b9
Andrew Pinzler <pinzler@gmail.com>
2012-09-08 23:33:22 -0400

    add letseat

--- letseat.php
+++ letseat.php
@@ -18,8 +18,42 @@ body {background-image:none; }
 $flight = $_REQUEST['flight'];	
 $seat = $_REQUEST['seat'];	
 
-		$querytest = "select * from $tbl_name where flight = '$flight' AND seat = '$seat';";
-		$resulttest = mysql_query($querytest);
-		$rowcounttest =  mysql_num_rows($resulttest);
-
+		$query = "select * from $tbl_name where flight = '$flight' AND seat = '$seat';";
 		
+		$result = mysql_query($query);
+	$rowcount =  mysql_num_rows($result);
+	$filledseats = array();
+	while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+	      $filledseats[] = array(
+	      'name' => $row['name'],
+	      'seat' => $row['seat'],
+	      'image' => $row['image'],
+	      'option1' => $row['option1'],
+	      'option2' => $row['option2'],
+	      'notes' => $row['notes'],
+	      'eaten' => $row['eaten'],
+		  'twitter' => $row['twitter']    
+	      );
+	}
+$tempname = $filledseats[0]['name'];
+$tempimage = $filledseats[0]['image'];
+?>
+
+<div class="row">
+		<div class="span10" style="margin-bottom:2em; "><p class="well">We are going to eat <?php echo $tempname; ?>! (That's some good eatin'!)<BR>
+<img src="<?php echo $tempimage; ?>"></p></div>
+		
+		</div>
+
+ <BR>
+<img src="http://www.whodoweeatnext.com/img/eatfirst.png"> <img src="http://www.whodoweeatnext.com/img/24b.png">
+
+<BR>
+
+<h1><a href = "flight.php?flight=Oceanic815">Who Do We Eat Next?!?!?!</a></h1>
+
+</div>
+
+<?php 
+include "lib/footer.php";
+?>

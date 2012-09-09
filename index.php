212fcf030f32518f30b3d9c03ab361ada2e81ea7
Andrew Pinzler <pinzler@gmail.com>
2012-09-08 23:41:30 -0400

    index updates

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 //header("location:flight.php?flight=Oceanic815");
 
 //echo $_ENV['OPENSHIFT_DB_HOST']."<BR>";
+include "lib/header.php";
 
 ?>
-Coming Soon!
+<style>
+body {background-image:none; }
+#main {padding-top:0; }
+</style>
+<div class="container" id="main">
+
+  <div class="row">
+    <div class="span10" style="margin-bottom:2em; "><p class="well">Coming Soon!
 <form name="input" action="flight.php" method="post">
 Flight: <input type="text" name="flight" />
 <input type="submit" value="Submit" />
-</form>
+</form></p></div>
+    
+    </div>
+
+    </div>
+
+<?php 
+include "lib/footer.php";
+?>
+
